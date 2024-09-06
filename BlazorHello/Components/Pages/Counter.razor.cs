e479f413fe320d3d75894214675dc1bc977d729f
shin.suzuki <shin.suzuki.dev@gmail.com>
2024-09-07 02:13:39 +0900

    add project blazor hello

---
+++ BlazorHello/Components/Pages/Counter.razor.cs
@@ -0,0 +1,17 @@
+﻿namespace BlazorHello.Components.Pages;
+
+public partial class Counter
+{
+    private int currentCount = 0;
+    private DateTime now = DateTime.Now;
+
+    private void IncrementCount()
+    {
+        currentCount++;
+    }
+
+    private void UpdateTime()
+    {
+        now = DateTime.Now;
+    }
+}
