29b97005d7ae99d94e94f155356f7500e7b6849d
shin.suzuki <shin.suzuki.dev@gmail.com>
2024-09-07 17:06:21 +0900

    add form input project.

---
+++ BlazorDataBind/Components/Pages/QuesrForm.razor.cs
@@ -0,0 +1,38 @@
+﻿namespace BlazorDataBind.Components.Pages
+{
+    public partial class QuesrForm
+    {
+        public string name { get; set; }
+        public int age { get; set; }
+
+
+        // select
+        public int prefecture { get; set; } 
+        public Dictionary<string, int> prefectureDic { get; set; } = new();
+
+
+        // checkbox, 数が多い場合は配列対応もあり
+        public bool likeCSharp { get; set; }
+        public bool likeVB{ get; set; }
+
+        public string Message { get; set; } 
+
+        protected override void OnInitialized()
+        {
+            prefectureDic.Add("北海道", 0);
+            prefectureDic.Add("東京", 1);
+            prefectureDic.Add("沖縄", 2);
+
+            // init
+            name = "名無し";
+            age = 20;
+            prefecture = 1;
+        }
+
+        private void onSubmit()
+        {
+            Message = "登録成功";
+        }
+
+    }
+}
---
+++ BlazorDataBind/Components/Pages/SimpleBind.razor.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorDataBind.Components.Pages
+{
+    public partial class SimpleBind
+    {
+        public string Title { get; set; }
+        public string Title2 { get; set; }
+    }
+}
