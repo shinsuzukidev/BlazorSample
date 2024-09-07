e96cb1d287e58ae783e072f44f9def42cdb7ffd2
shin.suzuki <shin.suzuki.dev@gmail.com>
2024-09-08 01:59:29 +0900

    add validation project.

---
+++ BlazorForm/Components/Address.cs
@@ -0,0 +1,21 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace BlazorForm.Components
+{
+    public class Address
+    {
+        [StringLength(7, ErrorMessage ="郵便番号は7桁です")]
+        public string zipCode {  get; set; }    
+        public string prefecture { get; set; }
+        [Required(ErrorMessage="電話番号は必須です")]
+        public string telephone { get; set; }
+        [Required(ErrorMessage = "名前を入力してください")]
+        public string name { get; set; }
+        [Range(10, 100, ErrorMessage ="年齢は10～100才までです")]
+        public int age { get; set; }
+        public bool isVisitDate { get; set; }
+        public DateTime? visitDate { get; set; }    
+
+    }
+}
---
+++ BlazorForm/Components/Pages/BlazorInput.razor.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace BlazorForm.Components.Pages
+{
+    public partial class BlazorInput
+    {
+        private Address addressModel = new();
+        public string? Message { get; set; }
+
+        public void onValidSubmit()
+        {
+            Message = $"thanks {addressModel.name}.";
+        }
+
+        private List<string> prefectureList = new List<string>()
+        {
+            "北海道",
+            "東京",
+            "沖縄"
+        };
+    }
+}
